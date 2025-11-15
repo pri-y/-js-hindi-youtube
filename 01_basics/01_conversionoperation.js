701b6138120e7fa0693e5fa72967304a2a17659d
Priyanka Gupta <priyankagupta1697@gmail.com>
2025-11-15 18:33:50 +0000

    conversion of datatype

---
+++ 01_basics/01_conversionoperation.js
@@ -0,0 +1,19 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueNumber = number(score)
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+//"33" =>> 33 
+//33abc =>> nan
+// tru => 1; falte => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLogged = boolean(isLoggedIn)
+console.log(booleanIsLogged);
+
+
